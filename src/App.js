0fcd1df9a09902f6ce84b3a0335d3263f7e4bb00
Cristhian Camilo Gomez Neira <ccgomezn@unal.edu.co>
2019-10-06 10:02:29 -0500

    search component created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Card} from '@material-ui/core';
+import SearchBox from "./components/searchBox";
+import Grid from "@material-ui/core/Grid";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Card>
+
+                <Grid container>
+                    <Grid item xs={4}>
+                        <SearchBox/>
+                    </Grid>
+
+                </Grid>
+
+            </Card>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/searchBox.js
@@ -0,0 +1,70 @@
+import TextField from "@material-ui/core/TextField";
+import clsx from "clsx";
+import {Button, FormControl, makeStyles} from "@material-ui/core";
+import React, {useState} from "react";
+import Grid from "@material-ui/core/Grid";
+
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+    },
+    dense: {
+        marginTop: theme.spacing(2),
+    },
+    menu: {
+        width: 200,
+    },
+    button: {
+        marginTop: '18px',
+    }
+}));
+
+
+function SearchBox() {
+    const classes = useStyles();
+
+    const [searchTest, setSearchText] = useState('');
+
+
+    const handleSubmit = (event) => {
+    };
+
+    const handleChange = (value) => {
+        setSearchText(value);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <Grid container>
+                <Grid item xs={12}>
+                    <TextField
+                        id="outlined-dense"
+                        label="Search..."
+                        className={clsx(classes.textField, classes.dense)}
+                        margin="dense"
+                        variant="outlined"
+                        value={searchTest}
+                        onChange={(event) => handleChange(event.target.value)}
+                    />
+                    <Button variant="contained" color="primary" type="submit" className={clsx(classes.button)}>
+                        Accept
+                    </Button>
+
+                </Grid>
+
+            </Grid>
+
+        </form>
+
+
+    );
+
+}
+
+export default SearchBox;
